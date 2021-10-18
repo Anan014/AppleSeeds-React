56f68af10c1add22cd5ca110b1fadc13dab2e001
Anan014 <ananamer95@gmail.com>
2021-10-18 15:41:21 +0300

    ex7.2 Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Index7D1 from "./components/7.State/ex7.1/Index7D1"
+import Index7D2 from "./components/7.State/ex7.2/Index7D2"
 
 function App() {
   return (
     <div>
-      <Index7D1/>
+      <Index7D2/>
     </div>
   );
 }
---
+++ src/components/7.State/ex7.2/Index7D2.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class Index7D2 extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isToggleOn: false
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        //change state
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <input onClick={this.handleClick}
+                type="button"
+                value={this.state.isToggleOn?"Show":"Hide"}
+                style={{width:"100px"}}
+                />
+                
+                <div style={{backgroundColor:"red",
+                 height:"100px",
+                 width:"100px",
+                 display:this.state.isToggleOn?"none":"block"
+                 }}>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Index7D2
